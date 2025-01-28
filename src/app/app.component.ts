1e47a62604c1c5fcb802b8aef444baa85e4a1370
spiros3p <spiros3p@gmail.com>
2025-01-28 22:56:07 +0100

    minor changes in names

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,4 @@ import { RouterModule } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'nx-angular-ssr';
-}
+export class AppComponent {}
