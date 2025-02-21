04a3ea1d7f7f9dc8e10eac0954f359c3b8f77863
spiros3p <spiros3p@gmail.com>
2025-02-21 18:28:32 +0100

    test: fix UT issue

--- src/app/core/i18n/components/language-switcher/language-switcher.component.spec.ts
+++ src/app/core/i18n/components/language-switcher/language-switcher.component.spec.ts
@@ -1,5 +1,6 @@
 import { Spectator, createComponentFactory } from '@ngneat/spectator/jest';
 
+import { TranslocoService } from '@jsverse/transloco';
 import { LanguageSwitcherComponent } from './language-switcher.component';
 
 describe('LanguageSwitcherComponent', () => {
@@ -7,6 +8,7 @@ describe('LanguageSwitcherComponent', () => {
   let component: LanguageSwitcherComponent;
   const createComponent = createComponentFactory({
     component: LanguageSwitcherComponent,
+    mocks: [TranslocoService],
   });
 
   beforeEach(() => {
